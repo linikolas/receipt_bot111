0b4730ecde32e991e9fa3c70608aa18c938338eb
linikolas <103752194+linikolas@users.noreply.github.com>
2025-06-05 01:42:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 
 # Токен вашего бота от @BotFather
 # Берется из переменной окружения для безопасности
-BOT_TOKEN = os.getenv('BOT_TOKEN', 'your_token_here')
+BOT_TOKEN = os.getenv('BOT_TOKEN', '7719346499:AAGsFv4w2W4sCjiID-aQCu6rbigb6dFuKu0')
 
 # Путь к CSV файлу для сохранения данных
 CSV_FILE_PATH = os.getenv('CSV_FILE_PATH', 'receipts.csv')
